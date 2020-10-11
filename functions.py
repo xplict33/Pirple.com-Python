5eb4755ae2b36d48fb3daf86e60a512c6be9a7c5
xplict33 <obiakarije@gmail.com>
2020-10-11 08:44:46 -0400

    Let's return to the music example from assignment one. Pick 3 of the attributes you listed. For our example we're going to say "Genre", "Artist" and "Year". Create a new Python file and create 3 functions with the same name as those attributes. So in this example we'd have one function named "genre" another named "artist" and another called "year".
    When someone calls any of those functions, that function should return the corresponding value for that attribute. For example, if we call the "Artist" function our function would return "Dave Brubeck". Yours should return whatever values make sense for your music choice.

---
+++ functions.py
@@ -0,0 +1,38 @@
+def artist():
+    name = "Remi Wolf"
+    print(name)
+    return name
+
+
+artist()
+
+
+def genre():
+    gen = "Pop"
+    print(gen)
+    return gen
+
+
+genre()
+
+
+def year():
+    num = 2020
+    print(num)
+    return num
+
+
+year()
+
+
+def boo():
+    a = 150
+    b = 60
+
+    if b > a:
+        return True
+    else:
+        return False
+
+
+print(boo())
