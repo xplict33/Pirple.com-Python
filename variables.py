eccd5211016013fadded69853ddd68b1939271dc
xplict33 <obiakarije@gmail.com>
2020-10-11 08:32:38 -0400

    What's your favorite song?
    Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
    
    In your text editor, create an empty file and name it main.py
    
    Now, within that file, list all of the attributes of the song, one after another, by creating variables for each attribute, and giving each variable a value. Here's an example:
    
    ```
    Genre = "Jazz"
    
    ```
    
    Give each variable its own line. Then, after you have listed the variables, print each one of them out.
    
     For example:
    
    ```
    
    Artist = "Dave Brubeck"
    Genre = "Jazz"
    DurationInSeconds = 328
    
    print(Artist)
    print(Genre)
    print(DurationInSeconds)
    
    ```
    Your actual assignment should be significantly longer than the example above. Think of as many characteristics of the song as you can. Try to use Strings,  Integers and Decimals (floats)!

---
+++ variables.py
@@ -0,0 +1,15 @@
+Artist = "Remi Wolf"
+Song = "Monte Carlo"
+Genre = "Pop"
+DurationInSeconds = 2.24
+Year = 2020
+RecordLabel = "Island Records"
+Director = "Austin Call"
+
+print(Artist)
+print(Song)
+print(Genre)
+print(DurationInSeconds)
+print(Year)
+print(RecordLabel)
+print(Director)
